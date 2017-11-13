d1ef1d7815b53ea44cda5d71c9a99f6e0067d93a
Ben <benbrown52@gmail.com>
2017-11-13 13:28:35 -0500

    No more shhhhh

--- pirunonstartup.sh
+++ pirunonstartup.sh
@@ -3,5 +3,6 @@
 DIR="/home/pi/ambilight"
 
 sh $DIR/pibuild.sh
-sudo ln -f $DIR/rpcVideo /usr/bin/rpcBuildAndRun
-sudo sh /usr/bin/rpcBuildAndRun
+sudo $DIR/rpcVideo
+#sudo ln -f $DIR/rpcVideo /usr/bin/rpcBuildAndRun
+#sudo sh /usr/bin/rpcBuildAndRun
