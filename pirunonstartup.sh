f99aabb9a7be26b1c005e19847de3104655977f9
Ben <benbrown52@gmail.com>
2017-11-13 13:54:51 -0500

    No git on startup

--- pirunonstartup.sh
+++ pirunonstartup.sh
@@ -3,8 +3,13 @@
 DIR="/home/pi/ambilight"
 cd $DIR
 CDIR=$(pwd); echo "Currently in $CDIR"
-sh pibuild.sh
-echo "Running..."
-sudo rpcVideo &
+if [ -f rpcVideo ]
+then
+    echo "Running..."
+    sudo rpcVideo &
+else
+    echo "rpvVideo not found. Exiting..."
+    exit
+fi
 #sudo ln -f $DIR/rpcVideo /usr/bin/rpcBuildAndRun
 #sudo sh /usr/bin/rpcBuildAndRun
