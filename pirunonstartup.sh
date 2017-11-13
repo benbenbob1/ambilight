b3df47eb8f391a283fce54cec9e4c5db548f85d6
Ben <benbrown52@gmail.com>
2017-11-13 16:15:37 -0500

    Updated script

--- pirunonstartup.sh
+++ pirunonstartup.sh
@@ -2,13 +2,14 @@
 
 DIR="/home/pi/ambilight"
 cd $DIR
+echo "Startup up pirunonstartup script" > /var/log/rpcVideo.log
 CDIR=$(pwd); echo "Currently in $CDIR"
 if [ -f rpcVideo ]
 then
-    echo "Running..."
+    echo "Running rpcVideo..." > /var/log/rpcVideo.log
     sudo ./rpcVideo > /var/log/rpcVideo.log
 else
-    echo "rpvVideo not found. Exiting..."
+    echo "rpvVideo not found. Exiting..." > /var/log/rpcVideo.log
     exit
 fi
 #sudo ln -f $DIR/rpcVideo /usr/bin/rpcBuildAndRun
